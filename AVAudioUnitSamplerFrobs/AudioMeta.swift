4997aeb3dfe00fcbf3570c5c8bc93a76e57dcde9
Gene De Lisa <gene@rockhoppertech.com>
2016-01-14 08:37:32 -0500

    first commit

---
+++ AVAudioUnitSamplerFrobs/AudioMeta.swift
@@ -0,0 +1,9 @@
+//
+//  AudioMeta.swift
+//  AVAudioUnitSamplerFrobs
+//
+//  Created by Gene De Lisa on 1/14/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import Foundation
---
+++ AVAudioUnitSamplerFrobs/SamplerSequence.swift
@@ -0,0 +1,9 @@
+//
+//  SamplerSequence.swift
+//  AVAudioUnitSamplerFrobs
+//
+//  Created by Gene De Lisa on 1/13/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import Foundation
